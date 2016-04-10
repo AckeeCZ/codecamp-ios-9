02a4f90c6ce47c6d2ac12e76a621919d4ea0d5b1
Petr Sima <ptea.sima@gmail.com>
2016-04-10 16:30:26 +0200

    Initial Commit

---
+++ CodecampScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodecampScrollView
+//
+//  Created by Petr Šíma on 10/04/16.
+//  Copyright © 2016 Petr Šíma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
